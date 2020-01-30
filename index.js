acec07b255336c88a9d2d5bd676d10ebdb29845a
Omer <omerkaraman@hotmail.co.uk>
2020-01-30 17:11:53 +0000

    Allowed header

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ mongoose.connect(
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-App-Signature");
   next();
 });
 
