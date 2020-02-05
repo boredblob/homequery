1a3eb519e3dc205f33aa3dcff060701e3cef02b2
Omer <omerkaraman@hotmail.co.uk>
2020-02-05 12:26:14 +0000

    added allow for token header

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const userRoute = require("./routes/user");
   
   app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, token");
     if (req.method === "OPTIONS") {
       res.header("Access-Control-Allow-Methods", "GET, POST, DELETE");
       return res.status(200).send("Woot");
